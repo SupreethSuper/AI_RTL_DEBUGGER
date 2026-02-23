2d4eb1e710134047523ad0a634105e991592baf1
supreeth <83527183+SupreethSuper@users.noreply.github.com>
2026-02-23 13:39:57 -0700

    Delete buffer.sv

--- buffer.sv
+++
@@ -1,8 +0,0 @@
-`include "GATE_PARAMS.vh"
-
-module buffer #(parameter WIDTH = NUM_GATES)(
-    input  logic [WIDTH-1:0] din,
-    output logic [WIDTH-1:0] dout
-);
-    assign dout = din;
-endmodule
